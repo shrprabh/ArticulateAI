cc5265e21c0a9cf351bfe5f4789a9af1a69e081c
shrprabh <shreyas.prabhakar97@gmail.com>
2023-10-31 17:34:34 -0500

    feat: Add login endpoint, implement validation and user/password checks

--- Backend/src/controllers/user-controllers.ts
+++ Backend/src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
-import { hash } from 'bcrypt';
+import { compare, hash } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -19,24 +19,52 @@ export const getAllUsers = async (
 };
 
 export const userSignUp = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    //get all users from the database
-    try {
-      // user signup
-      const{name,email,password}=req.body;
-      const hashedpassword=await hash(password,10);//10 will do the 10 folds of encryption strategy
-      // we can't store the password as it is in the backend so we need to encrypt the password
-      // so we will use a library called bcrypt
-      const user=new User({name,email,password:hashedpassword});
-      await user.save();
-      const users = await User.find();
-      // since id is in object format
-      return res.status(200).json({ message: "OK", id:user._id.toString() });
-    } catch (error) {
-      console.log(error);
-      return res.status(200).json({ message: "ERROR", cause: error.message });
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //get all users from the database
+  try {
+    console.log("Inside User Sign Up");
+    // user signup
+    const { name, email, password } = req.body;
+    //To validate if user already exist in database
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User Already Registered");
+    const hashedpassword = await hash(password, 10); //10 will do the 10 folds of encryption strategy
+    // we can't store the password as it is in the backend so we need to encrypt the password
+    // so we will use a library called bcrypt
+    const user = new User({ name, email, password: hashedpassword });
+    await user.save();
+    const users = await User.find();
+    // since id is in object format
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    console.log("USer exists",user);
+    if (!user) {
+      return res.status(401).send("User Not Registered");
     }
-  };
+    // validating the hashed password in database
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Incorrect Password");
+    }
+    return res.status(200).send({message:"OK",id:user._id})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "ERROR", cause: error.message });
+  }
+};
--- Backend/src/utils/validators.ts
+++ Backend/src/utils/validators.ts
@@ -1,32 +1,34 @@
 import { NextFunction, Request, Response } from "express";
 import { ValidationChain, body, validationResult } from "express-validator";
 
-//
+// validation middleware
 export const validate = (validations: ValidationChain[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    
     for (let validation of validations) {
       const result = await validation.run(req);
-      //console.log("Validation result:", result.mapped()); 
+      //console.log("Validation result:", result.mapped());
     }
     console.log("Inside validate function");
     const errors = validationResult(req);
-    console.log("Errors",errors.array());
+    console.log("Errors", errors.array());
     if (errors.isEmpty()) {
       //if there are no errors then move to next function
-      res.send("Validation passed");
+      // res.send("Validation passed"); //removing this to resolve the issue where after this userssignn up will be called leadling
+      // into call of other middleware where res.send is used
       return next();
     }
     return res.status(422).json({ errors: errors.array() });
   };
 };
-//Middleware where we can validate the user first before if we need to store information in DB
-export const signupValidator = [
-  body("name").notEmpty().withMessage("Name is required"),
+export const loginValidator = [
   body("email").trim().isEmail().withMessage("Email is Required"),
   body("password")
     .trim()
     .isLength({ min: 6 })
     .withMessage("Password should contain atleast 6 characters"),
 ];
-
+//Middleware where we can validate the user first before if we need to store information in DB
+export const signupValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  ...loginValidator
+]
