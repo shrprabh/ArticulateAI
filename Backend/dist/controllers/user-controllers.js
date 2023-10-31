fad9a7f5f8629c8fee7622248274c45a1333d1d5
shrprabh <shreyas.prabhakar97@gmail.com>
2023-10-31 17:36:04 -0500

    build: files which are converted to js file after build

--- Backend/dist/controllers/user-controllers.js
+++ Backend/dist/controllers/user-controllers.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import { hash } from 'bcrypt';
+import { compare, hash } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     //get all users from the database
     try {
@@ -18,6 +18,10 @@ export const userSignUp = async (req, res, next) => {
         console.log("Inside User Sign Up");
         // user signup
         const { name, email, password } = req.body;
+        //To validate if user already exist in database
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User Already Registered");
         const hashedpassword = await hash(password, 10); //10 will do the 10 folds of encryption strategy
         // we can't store the password as it is in the backend so we need to encrypt the password
         // so we will use a library called bcrypt
@@ -29,7 +33,27 @@ export const userSignUp = async (req, res, next) => {
     }
     catch (error) {
         console.log(error);
-        return res.status(200).json({ message: "ERROR", cause: error.message });
+        return res.status(500).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        console.log("USer exists", user);
+        if (!user) {
+            return res.status(401).send("User Not Registered");
+        }
+        // validating the hashed password in database
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        return res.status(200).send({ message: "OK", id: user._id });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "ERROR", cause: error.message });
     }
 };
 //# sourceMappingURL=user-controllers.js.map
