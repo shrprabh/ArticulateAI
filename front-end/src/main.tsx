ffaa7f6dec4e6bee1637822920c75b2dbb1ef7ce
shrprabh <shreyas.prabhakar97@gmail.com>
2023-11-15 16:08:32 -0600

    feat(login backend conn): establishing connection with backend for login, adding toasters and allowing cookies

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,6 +5,10 @@ import "./index.css";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import axios from "axios";
+import {Toaster} from "react-hot-toast";
+axios.defaults.baseURL="http://localhost:5004/api/v1/";
+axios.defaults.withCredentials=true; // it will allow exchanging cookies from backend and allow setting cookies directly from backend
 const theme = createTheme({
   typography: {
     fontFamily: "Roboto slab,serif",
@@ -16,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <AuthProvider>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
+        <Toaster position="top-right"></Toaster>
         <App />
       </ThemeProvider>
     </BrowserRouter>
--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -3,17 +3,26 @@ import React from "react";
 import robot from "../assets/robow.png";
 import { CustomizedInput } from "../components/shared/CustomizedInput";
 import { RiLoginBoxLine } from "react-icons/ri";
+import { useAuth } from "../context/AuthContext";
+import toast from "react-hot-toast";
 export const Login = () => {
+  const auth = useAuth();
   //to get the form data
-  const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     // to prevent the default behaviour
     e.preventDefault();
-    const formData= new FormData(e.currentTarget);
-    const email=formData.get("email");
-    const password=formData.get("password");
-    console.log(email,password)
-
-  }
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    console.log(email, password);
+    try {
+      toast.loading("Signing In", { id: "login" }); // id should be same for inside all of the toast
+      await auth?.login(email, password);
+      toast.success("Signed in Succesfully", { id: "login" });
+    } catch (error) {
+      toast.error("Signing in Failed", { id: "login" });
+    }
+  };
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
@@ -29,7 +38,7 @@ export const Login = () => {
         mt={16}
       >
         <form
-          onSubmit={(handleSubmit)}
+          onSubmit={handleSubmit}
           style={{
             margin: "auto",
             padding: "30px",
