f743f055863df1f761ca2ae948d00f642aaf5889
shrprabh <shreyas.prabhakar97@gmail.com>
2023-11-22 14:25:22 -0600

    feat(open ai):adding routes and validation and storing the data in backend and giving api api response

--- Backend/src/utils/validators.ts
+++ Backend/src/utils/validators.ts
@@ -32,3 +32,7 @@ export const signupValidator = [
   body("name").notEmpty().withMessage("Name is required"),
   ...loginValidator
 ]
+
+export const chatCompletionValidator = [
+  body("message").notEmpty().withMessage("Message is required"),
+]
